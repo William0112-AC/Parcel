aa5b3a52b4eaefaddf0725b1553f538ccb03174c
Elizabeth <alex.26.azul@gmail.com>
2023-04-20 17:57:34 -0600

    logica del proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 const $app = document.getElementById("app");
 const API = "htps://api.escuelajs.co//api/v1/pruducts?offset=0&limit=10"
 
+const main= async () => {
+
+    const response = await fetch(API);  
+    const products = await response.json();
+
+    const output = products?. map ((product) => {
+    
+    return'
+    <article class="Card">
+    <img src="${product.images[0]}">
+     <h2>
+     ${product.title} <small>Precio $ ${product.price}</small> 
+    </h2>
+    </article>
+    ';
+    
+    }).join(""); You,1 second ago Uncommitted changes
+    let newItem = document.createElement("section");
+     newItem.classList.add("Items");
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+}
+main();
